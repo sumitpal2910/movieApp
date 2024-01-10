ad55e56d92cf061a19e995fab2974b764f49a2c1
Sumit Pal <sumit@nickit.in>
2024-01-10 15:51:11 +0530

    add functionality to navbar

--- app/src/main/java/com/eightbit/blog/MainActivity.java
+++ app/src/main/java/com/eightbit/blog/MainActivity.java
@@ -1,20 +1,94 @@
 package com.eightbit.blog;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.eightbit.blog.fragment.ProfileFragment;
 import com.eightbit.blog.models.RequestDto;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RequestDto<Object> dto = new RequestDto<>();
-        Object data = dto.getData();
+        setView();
 
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.navProfile:
+                        loadFragment(new ProfileFragment());
+                        break;
+                    case R.id.navMovie:
+                        //
+                        break;
+                    case R.id.navSeries:
+                        //
+                        break;
+                    case R.id.navLogout:
+                        //
+                        break;
+                }
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                return true;
+            }
+        });
+
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+
+        FragmentTransaction ft = fm.beginTransaction();
+
+        ft.add(R.id.container, fragment);
+
+        ft.commit();
+    }
+
+    private void setView() {
+        navigationView = findViewById(R.id.navigationView);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        toolbar = findViewById(R.id.toolbar);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            getOnBackPressedDispatcher().onBackPressed();
+        }
     }
 }
